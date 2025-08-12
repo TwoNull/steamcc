a33fd25ef9819d7407645784a9f54b012a9da2e3
TwoNull <115444279+TwoNull@users.noreply.github.com>
2025-08-12 15:54:07 -0400

    Update types.go

--- types/types.go
+++ types/types.go
@@ -3,6 +3,6 @@ package types
 type User struct {
 	AccountName string
 	PersonaName string
-	Steam64     string
-	AutoLogin   bool
+	Steam64     string // Steam64 ID
+	AutoLogin   bool   // Whether user is likely to have a valid Connect Cache JWT saved
 }
